8970dd8d827bb9ee5e271aac5fbf9fd97e6f5ef4
dilya <1dilnora777@gmail.com>
2022-11-09 00:16:29 +0500

    task003 added

---
+++ task003/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число и выдает, 
+// является ли число четным (делится ли оно без остатка).
+
+Console.WriteLine("Enter num: ");
+
+i = int.Parsel(Console.Read());
+
+if (i % 2==0)
+{
+    Console.WriteLine("The number is EVEN");
+}
+else 
+{
+    Console.WriteLine("The number is ODD");
+}
